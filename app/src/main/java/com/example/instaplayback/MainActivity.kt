106fe844aa40bc4738d3c7f1d8da00f0f192c31a
cheerfulpianissimo <sooraj2642@gmail.com>
2022-05-12 19:53:47 +0530

    Initial Commit. Playback on device.

---
+++ app/src/main/java/com/example/instaplayback/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.instaplayback
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.*
+import android.media.audiofx.AcousticEchoCanceler
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+        private const val STORAGE_PERMISSION_CODE = 101
+    }
+
+    var isRecording = true
+    var am: AudioManager? = null
+    var record: AudioRecord? = null
+    var track: AudioTrack? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initRecordAndTrack()
+        am = this.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        // Defining Buttons
+        val playbackB: Button? = findViewById(R.id.playback)
+        object : Thread() {
+            override fun run() {
+                recordAndPlay()
+            }
+        }.start()
+
+        // Set Buttons on Click Listeners
+        playbackB?.setOnClickListener {
+            if (!isRecording) {
+                startRecordAndPlay()
+            }else{
+                stopRecordAndPlay()
+            }
+        }
+    }
+
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun initRecordAndTrack() {
+        val min = AudioRecord.getMinBufferSize(
+            8000,
+            AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        checkPermission(
+            Manifest.permission.RECORD_AUDIO,
+            STORAGE_PERMISSION_CODE)
+        record = AudioRecord(
+            MediaRecorder.AudioSource.VOICE_PERFORMANCE,
+            8000,
+            AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            min
+        )
+        /*if (AcousticEchoCanceler.isAvailable()) {
+            val echoCanceler = AcousticEchoCanceler.create(record!!.audioSessionId)
+            echoCanceler.enabled = true
+        }*/
+        val maxJitter = AudioTrack.getMinBufferSize(
+            8000,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        track = AudioTrack(
+            AudioManager.MODE_IN_COMMUNICATION,
+            8000,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            maxJitter,
+            AudioTrack.MODE_STREAM
+        )
+    }
+
+    private fun recordAndPlay() {
+        val lin = ShortArray(1024)
+        var num = 0
+        am!!.mode = AudioManager.MODE_IN_COMMUNICATION
+        while (true) {
+            if (isRecording) {
+                num = record!!.read(lin, 0, 1024)
+                track!!.write(lin, 0, num)
+            }
+        }
+    }
+
+    private fun startRecordAndPlay() {
+        record!!.startRecording()
+        track!!.play()
+        isRecording = true
+    }
+
+    private fun stopRecordAndPlay() {
+        record!!.stop()
+        track!!.pause()
+        isRecording = false
+    }
+}
